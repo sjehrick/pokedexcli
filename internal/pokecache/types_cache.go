ebc4c0e2c6f52368b2c29c29c26a245d8ebc9bfb
samuel <samuel.ehrick@gmail.com>
2026-04-13 13:56:22 -0400

    Created new internal package for pokecache and defined cache types

---
+++ internal/pokecache/types_cache.go
@@ -0,0 +1,16 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	Mu    sync.Mutex
+	Entry map[string]cacheEntry
+}
