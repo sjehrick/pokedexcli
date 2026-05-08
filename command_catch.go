51cfc8df746702e114999c4160a77987136a875b
samuel <samuel.ehrick@gmail.com>
2026-05-08 16:38:42 -0400

    built out basic catch logic to include rand function for probibility based catch or escape

--- command_catch.go
+++ command_catch.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
 func commandCatch(cfg *config, p string) error {
+	if p == "" {
+		return errors.New("A valid pokemon name must be provided")
+	}
+
+	fmt.Println("Throwing a Pokeball at " + p + "...")
+
+	respPokemon, err := cfg.pokeapiClient.ListPokemonStats(p)
+	if err != nil {
+		return err
+	}
+
+	// 675 is based on the ~10% increase above max base experience of Blissey at 608
+	randCatch := rand.Intn(675)
+
+	if respPokemon.BaseExperience <= randCatch {
+		fmt.Println(p + " was caught!")
+		return nil
+	}
+
+	fmt.Println(p + " escaped!")
+
 	return nil
 }
